8c858f3d75510b899f12adf020864d19299596d0
Kamurai <KamuraiBlue25@gmail.com>
2017-09-10 17:11:27 -0400

    Update 20170910

---
+++ Python/ex3.py
@@ -0,0 +1,33 @@
+print ""
+print "I will now count my chickens:"
+
+#addition and division
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+print "Now I will count the eggs:"
+
+#addition, subtraction, modulation and division
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+#addition, less than comparison and subtraction
+print 3.0 + 2.0 < 5.0 - 7.0
+
+#print addition on the fly
+print "What is 3 + 2?", 3.0 + 2.0
+#print subtraction on the fly
+print "What is 5 - 7?", 5.0 - 7.0
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+#greater than comparison on the fly
+print "Is it greater?", 5.0 > -2.0
+#greater than or equal to comparison on the fly
+print "Is it greater or equal?", 5.0 >= -2.0
+#less than or equal to comparison on the fly
+print "Is it less or equal?", 5.0 <= -2.0
+print ""
---
+++ Python/ex4.py
@@ -0,0 +1,28 @@
+print "";
+
+#number of cars
+cars = 100
+#number of passengers per car
+space_in_a_car = 4.0
+#number of drivers
+drivers = 30
+#number of total passengers
+passengers = 90
+#cars without drivers
+cars_not_driven = cars - drivers
+#cars with drivers
+cars_driven = drivers
+#number of seats in all cars with drivers
+carpool_capacity = cars_driven * space_in_a_car
+#supposed number of passengers per car
+average_passengers_per_car = passengers / cars_driven
+
+
+print "There are", cars, "cars available."
+print "There are only", drivers, "drivers available."
+print "There will be", cars_not_driven, "empty cars today."
+print "We can transport", carpool_capacity, "people today."
+print "We have", passengers, "to carpool today."
+print "We need to put about", average_passengers_per_car, "in each car."
+
+print "";
